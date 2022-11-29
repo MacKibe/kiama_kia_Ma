a3a7f649c06928afc9137ab1a64061ccfc8edd48
mackibe <ndichujames2030@gmail.com>
2022-11-29 17:19:43 +0300

    updated the images container and added a flex display and a slider to show the photos

--- scripts/index.js
+++ scripts/index.js
@@ -25,22 +25,22 @@ function plusDivs(n) {
   showDivs(slideIndex += n);
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mattressPics");
-  var y = document.getElementsByClassName("firewoodPics");
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mattressPics");
+//   var y = document.getElementsByClassName("firewoodPics");
 
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   x[slideIndex-1].style.display = "block";  
 
-  if (n > y.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = y.length}
-  for (i = 0; i < y.length; i++) {
-    y[i].style.display = "none";  
-  }
-  y[slideIndex-1].style.display = "block"; 
-}
+//   if (n > y.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = y.length}
+//   for (i = 0; i < y.length; i++) {
+//     y[i].style.display = "none";  
+//   }
+//   y[slideIndex-1].style.display = "block"; 
+// }
