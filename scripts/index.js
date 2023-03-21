4beadc7125edb283d5f6060af05cbef9d9e18f5b
mackibe <ndichujames2030@gmail.com>
2023-03-21 07:39:46 +0300

    added the buy me coffee

--- scripts/index.js
+++ scripts/index.js
@@ -43,33 +43,54 @@ function nextSlide() {
 // }
 
 window.onload = function(){
-    //
-    //Collect the 2 key parameters of an intersection observer object
-    //
-    //1. The callback function to call whenever we scroll
-    const callback = (entries, observer)=>{
-       //
-       // For each observed entry-----
-       for(const entry of entries){
-           //
-           //----get the target element (as an audio)
-           const audio =entry.target;
-           //
-           //----if the audio intersets with the view port....
-           if (entry.isIntersecting) {
-               //
-               //...play the audio
-               audio.play();
-           }
-           //
-           //...otherwise
-           else {
-               //
-               //Pause the audio
-               audio.pause();
-           };
-       }
-   };
+  //
+  //Collect the 2 key parameters of an intersection observer object
+  //
+  //1. The callback function to call whenever we scroll
+  const callback = (entries, observer)=>{
+     //
+     //For each observed entry-----
+     for(const entry of entries){
+         //
+         //----get the target element (as an audio)
+         const audio =entry.target;
+         //
+         //----if the audio ntersets with the view port....
+         if (entry.isIntersecting) {
+             //
+             //...play the audio
+             audio.play();
+         }
+         //
+         //...otherwise
+         else {
+             //
+             //Pause the audio
+             audio.pause();
+         };
+     }
+ };
+ //
+ //2. The options for controlling the intersection.
+ //In our case the options are:-
+ //- There is intersection if at least half of the audio is visible
+ const options = {threshold:.5};
+ //
+ //Create a new intersection observer object
+ const observer = new IntersectionObserver(callback, options);
+
+ //Collect all the audio on this page, as an array
+ const audios = Array.from(document.querySelectorAll('audio'));
+ //
+ //For each audio...
+ for(const audio of audios){
+     //
+     //...mute the audio (otherwise it won't play!)
+     audio.muted = flase;
+     //
+     //...put the audio in the list of observed entries
+     observer.observe(audio);
+ }
 };
 
 // Get all menu items and sections
