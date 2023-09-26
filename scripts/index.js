f823611b340270710a10a3e4d9b56023a0d632cf
mackibe <ndichujames2030@gmail.com>
2023-09-26 07:19:10 +0300

    removed all blank html pages

--- scripts/index.js
+++ scripts/index.js
@@ -1,99 +1,95 @@
-
 // Assigning my html elements variables
 const primaryNav = document.querySelector(".primary-nav");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
-
 // Adding event listener to the button whent the user taps the button.
 navToggle.addEventListener("click", () => {
-    const visibility = primaryNav.getAttribute('data-visible');
+  const visibility = primaryNav.getAttribute("data-visible");
 
-    if (visibility === "false") {
-        primaryNav.setAttribute('data-visible', true);
-        navToggle.setAttribute('aria-expanded', true);
-    } else if (visibility === "true") {
-        primaryNav.setAttribute('data-visible', false);
-        navToggle.setAttribute('aria-expanded', false);
-    }
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
 });
-var slides = document.querySelectorAll('.graduationPics');
+var slides = document.querySelectorAll(".graduationPics");
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide, 2000);
 
 function nextSlide() {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
 }
 
-window.onload = function(){
-  //
-  //Collect the 2 key parameters of an intersection observer object
-  //
-  //1. The callback function to call whenever we scroll
-  const callback = (entries, observer)=>{
-     //
-     //For each observed entry-----
-     for(const entry of entries){
-         //
-         //----get the target element (as an audio)
-         const audio =entry.target;
-         //
-         //----if the audio ntersets with the view port....
-         if (entry.isIntersecting) {
-             //
-             //...play the audio
-             audio.play();
-         }
-         //
-         //...otherwise
-         else {
-             //
-             //Pause the audio
-             audio.pause();
-         };
-     }
- };
- //
- //2. The options for controlling the intersection.
- //In our case the options are:-
- //- There is intersection if at least half of the audio is visible
- const options = {threshold:.5};
- //
- //Create a new intersection observer object
- const observer = new IntersectionObserver(callback, options);
+window.onload = function () {
+  // Intersection Observer for videos
+  const videoCallback = (entries, observer) => {
+    for (const entry of entries) {
+      const video = entry.target;
+      if (entry.isIntersecting) {
+        video.play();
+        video.muted = true;
+      } else {
+        video.pause();
+      }
+    }
+  };
 
- //Collect all the audio on this page, as an array
- const audios = Array.from(document.querySelectorAll('audio'));
- //
- //For each audio...
- for(const audio of audios){
-     //
-     //...mute the audio (otherwise it won't play!)
-     audio.muted = flase;
-     //
-     //...put the audio in the list of observed entries
-     observer.observe(audio);
- }
+  const videoOptions = { threshold: 1 };
+  const videoObserver = new IntersectionObserver(videoCallback, videoOptions);
+
+  const videos = Array.from(document.getElementsByTagName("video"));
+
+  for (const video of videos) {
+    video.muted = false;
+    videoObserver.observe(video);
+  }
+
+  // Intersection Observer for audio
+  const audioCallback = (entries, observer) => {
+    for (const entry of entries) {
+      const audio = entry.target;
+      if (entry.isIntersecting) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+    }
+  };
+
+  const audioOptions = { threshold: 1 };
+  const audioObserver = new IntersectionObserver(audioCallback, audioOptions);
+
+  const audios = Array.from(document.getElementsByTagName("audio"));
+
+  for (const audio of audios) {
+    audio.muted = false;
+    audioObserver.observe(audio);
+  }
 };
 
 // Get all menu items and sections
-const menuItems = document.querySelectorAll('.side-nav-menu li a');
-const sections = document.querySelectorAll('.section');
+const menuItems = document.querySelectorAll(".side-nav-menu li a");
+const sections = document.querySelectorAll(".section");
 
 // Create an IntersectionObserver to detect when a section is in view
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
     // If a section is in view, add the "active" class to its corresponding menu item
-    const id = entry.target.getAttribute('id');
-    const menuItem = document.querySelector(`.side-nav-menu li a[href="#${id}"]`);
+    const id = entry.target.getAttribute("id");
+    const menuItem = document.querySelector(
+      `.side-nav-menu li a[href="#${id}"]`
+    );
     if (entry.isIntersecting) {
-      menuItem.classList.add('active');
+      menuItem.classList.add("active");
     } else {
-      menuItem.classList.remove('active');
+      menuItem.classList.remove("active");
     }
   });
 });
 
 // Observe all sections
-sections.forEach(section => observer.observe(section));
+sections.forEach((section) => observer.observe(section));
